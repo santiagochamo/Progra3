401beb27cfe0e5aa095bd32665039430e005157b
bautistafernandezsasso <bautifernandezs@gmail.com>
2022-09-12 17:41:34 -0300

    header

--- proyectointegrador/src/components/Header/Header.js
+++ proyectointegrador/src/components/Header/Header.js
@@ -1,22 +1,57 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
 
-function Header(){
-    return(
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About</Link>
-                    </li>
-                </ul> 
-            </nav>
-        </header>
-    )
+import React, {Component} from "react";
+import {Link} from "react-router-dom"
+import Search from "../Search/Search"; //arreglar esto
+
+
+class Header extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            valor: "",
+            data: []
+        }
+    }
+
+    buscarPeliculas(nombre){
+        if (nombre !== "") {
+            fetch(`https://api.themoviedb.org/3/search/movie?api_key=0002daaf86f106b6b8226fa0a789628f&query=${nombre}`)
+        .then(resp => resp.json())
+        .then(data => this.setState({
+            data: data.results,
+
+        }))
+        .catch(error => console.log(error))
+            
+        }else{
+            <p>No hay resultados que coincidan con esta busqueda. </p>
+        }
+        
+    }
+    render(){
+        return(
+        <>
+        <nav>
+                <ul className="user">
+                    <li> <Link to = {`/`}> <img src="./public/img/logoPelicula.png" alt="logo" /> </Link></li>
+                </ul>
+                <div className='search-box'>
+                        <Search buscar ={(nombre) => this.buscarPeliculas(nombre)}/> 
+                        {
+                            this.state.data.length > 0 ? this.state.data.map((favoritos) => <h2>{favoritos.title}</h2>) : ""
+                        }
+                </div>
+                <ul className="main-nav">
+                    <li> <Link to ={`/`}>Home</Link></li>
+                    <li> <Link to ={`/favoritos`}>Favoritos</Link></li>
+                </ul>
+        </nav>
+        </>
+            
+            )
+
+    }
+    
 }
-
 export default Header
     
