5da7d7f82c35c7db4e1412e347743407c0f49248
bautistafernandezsasso <bautifernandezs@gmail.com>
2022-09-12 15:27:04 -0300

    details

--- proyectointegrador/src/Screens/Details/Details.js
+++ proyectointegrador/src/Screens/Details/Details.js
@@ -1,29 +1,43 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import DetalleCard from "../DetalleCard/DetalleCard"
+import {Link} from 'react-router-dom';
 
-class Details extends Component{
+class Detalle extends Component{
     constructor(props){
         super(props)
         this.state={
-            id: props.match.params.id,
-            character:{}
+           id: Number(props.match.params.id),
+           peliculas: []
         }
+        console.log(this.state.id)
     }
 
     componentDidMount(){
-        fetch(`https://rickandmortyapi.com/api/character/${this.state.id}`)
-        .then(resp => resp.json())
-        .then(data => this.setState({
-            character: data
-        }))
-        .catch(err => console.log(err))
+        let peliculasOk = []
+        let url = `https://api.themoviedb.org/3/movie/${this.state.id}?api_key=63cdfcbb1edb0e2c2331f8b2cb24ba9b`
+        fetch(url)
+            .then(response => response.json())
+            .then(data => peliculasOk.push(data))
+            .then(data => this.setState(
+                {
+                    peliculas: peliculasOk }
+                    
+            ))
+            .catch(error => console.log('El error es' + error))
+            console.log(this.state.peliculas)
     }
+    
+
     render(){
         return(
-            <div>
-                <img src={this.state.character.image} />
-            </div>
+            <React.Fragment>
+           {this.state.peliculas.map((unDetalle, idx) => <DetalleCard key={unDetalle + idx} datosDetalle={unDetalle} />)}
+           </React.Fragment>
+
         )
     }
+
 }
 
-export default Details
+export default Detalle
+
