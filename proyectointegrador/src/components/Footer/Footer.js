61fb1dd2b034c06f2565affbfda6bab598f013c9
bautistafernandezsasso <bautifernandezs@gmail.com>
2022-09-12 20:46:38 -0300

    footer

---
+++ proyectointegrador/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styless.css'
+
+function Footer() {
+  return (
+    <footer>
+        <img  src="./public/img/logoPelicula.png" alt="logo"></img>
+        <p className="fondo">Copyright © - Todos los derechos reservados</p>
+        <p >Bautista Linares, Santiago Chamo, Bautista Fernandez Sasso </p>
+    </footer>
+  )
+}
+
+export default Footer
