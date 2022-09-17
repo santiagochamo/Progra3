bae947f175c82893951d6b2f286d15eabd858288
Bauti Linares <bato@MacBook-Pro-de-Bauti.local>
2022-09-17 14:58:31 -0300

    detalle series y arreglos

---
+++ proyectointegrador/src/components/DetailSeries/DetailSeries.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import SeriesCard from '../../Screens/SeriesCard/SeriesCard'
+
+
+class Detalle extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+           id: Number(props.match.params.id),
+           peliculas: []
+        }
+        console.log(this.state.id)
+    }
+
+    componentDidMount(){
+        let peliculasOk = []
+        let url = `https://api.themoviedb.org/3/movie/${this.state.id}?api_key=63cdfcbb1edb0e2c2331f8b2cb24ba9b`
+        fetch(url)
+            .then(response => response.json())
+            .then(data => peliculasOk.push(data))
+            .then(data => this.setState(
+                {
+                    peliculas: peliculasOk }
+                    
+            ))
+            .catch(error => console.log('El error es' + error))
+            console.log(this.state.peliculas)
+    }
+    
+
+    render(){
+        return(
+            <React.Fragment>
+           {this.state.peliculas.map((unDetalle, idx) => <SeriesCard key={unDetalle + idx} datosDetalle={unDetalle} />)}
+           </React.Fragment>
+
+        )
+    }
+
+}
+
+export default Detalle
+
